352fd1f647b8d606a37f046b182cf080c41373ef
Nawras Nazar <nawras.nzar@gmail.com>
2024-08-22 14:09:25 +0300

    move newpost submit handler to NewPost.jsx

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,17 +1,25 @@
 import classes from './NewPost.module.css'
 
-function NewPost({onBodyChange, onAuthorChange, onCancel}) {
+function NewPost({onCancel}) {
     
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const bodyText = formData.get('bodyText');
+        const authorName = formData.get('authorName');
+        console.log(bodyText)
+        console.log(authorName)
+    }
     return (
         <>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={handleSubmit}>
             <p>
                 <label htmlFor='body'>Text</label>
-                <textarea id='body' required-rows={3} onChange={onBodyChange} />
+                <textarea id='body' required-rows={3} required name='bodyText' />
             </p>
             <p>
                 <label htmlFor='name'>Your name</label>
-                <input type='text' id='name' required onChange={onAuthorChange} />
+                <input type='text' id='name' required name='authorName' />
             </p>
             <p className={classes.actions}>
                 <button type='button' onClick={onCancel}>Cancel</button>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,40 +1,21 @@
-import { useState } from 'react';
-
 import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({isPosting, onStopPosting}) {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  function modalSubmitHandler(event) {
-    console.log("Submitting the form!")
-  }
 
   let modalContent;
   modalContent = isPosting? (<Modal onClose={onStopPosting}>
         <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
           onCancel={onStopPosting}
-          onSubmit={modalSubmitHandler}
         />
       </Modal>): ""; 
   return (
     <>
       {modalContent}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
+        <Post />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
